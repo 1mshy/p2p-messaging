63a7a15acb6d6acaa680ae3196895df3f51a0b97
imshy <imshi1@protonmail.com>
2024-05-02 10:57:27 -0400

    seperated chatpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,15 @@
-import {useState} from "react";
-import {invoke} from "@tauri-apps/api/tauri";
 import "./App.css";
+import ChatPage from "./messaging/ChatPage.tsx";
 
 function App() {
-    const [greetMsg, setGreetMsg] = useState("");
-    const [ip, setIp] = useState("");
-    const [message, setMessage] = useState("");
-
-    async function greet() {
-        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-        setGreetMsg(await invoke("message", {name: message}));
-    }
-
-    async function request_ip() {
-        setIp(await invoke("request_ip"))
-    }
 
     return (
         <div className="container">
             <h1>Welcome to P2P Messaging!</h1>
             <p>Send a message to start.</p>
 
-            <form
-                className="row"
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    greet()
-                    request_ip();
-                    invoke("register", {name: message})
-                }}
-            >
-                <input
-                    id="greet-input"
-                    onChange={(e) => setMessage(e.currentTarget.value)}
-                    placeholder="Enter a Message..."
-                />
-                <button type="submit">Send</button>
-            </form>
+            <ChatPage/>
 
-            <p>{greetMsg}</p>
-            {ip !== "" && <p>*Note your ip has been tracked: {ip}</p>}
         </div>
     );
 }
---
+++ src/messaging/ChatPage.tsx
@@ -0,0 +1,45 @@
+import {useState} from "react";
+import {invoke} from "@tauri-apps/api/tauri";
+
+function ChatPage() {
+    const [greetMsg, setGreetMsg] = useState("");
+    const [ip, setIp] = useState("");
+    const [message, setMessage] = useState("");
+
+    async function greet() {
+        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+        setGreetMsg(await invoke("message", {name: message}));
+    }
+
+    async function request_ip() {
+        setIp(await invoke("request_ip"))
+    }
+
+    return (
+        <div>
+            <form
+                className="row"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    greet()
+                    request_ip();
+                    invoke("register", {name: message})
+                }}
+            >
+                <input
+                    id="greet-input"
+                    onChange={(e) => setMessage(e.currentTarget.value)}
+                    placeholder="Enter a Message..."
+                />
+                <button type="submit">Send</button>
+            </form>
+
+            <p>{greetMsg}</p>
+            {
+                ip !== "" && <p>*Note your ip has been tracked: {ip}</p>
+            }
+        </div>
+    )
+}
+
+export default ChatPage;
